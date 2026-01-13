6adaf9372b8e0ff3f7965386a0dd2299dfe786f6
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2026-01-13 16:40:01 -0600

    feat: finished header

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,8 +1,31 @@
 import { ContextConsumer } from "@lit/context";
-import { html, LitElement } from "lit";
+import { css, html, LitElement } from "lit";
 import { clockContext } from "../context/clock-context";
 
 export class AppHeader extends LitElement {
+    static styles = css`
+    :host {
+        padding: 0 0.5rem;
+    }
+    .header{
+        padding: 1.25rem 0.75rem 0.375rem 0.75rem;
+        display: flex;
+        flex-direction: column;
+
+        & .time {
+            font-size: 1rem;
+            margin-left: 0.5rem;
+            color: black;
+        }
+
+        & .greeting {
+            font-size: 1.75rem;
+            line-height: 1.75rem;
+            font-weight: 600;
+            color: black;
+        }
+    }`;
+
     static get properties() {
         return {
             greetingMessage: {
@@ -38,7 +61,6 @@ export class AppHeader extends LitElement {
                 const { greeting, time } = value;
                 this.greetingMessage = greeting;
                 this.time = time;
-
             }
         });
     }
@@ -50,7 +72,10 @@ export class AppHeader extends LitElement {
 
     render() {
         return html`
-            <div>${this.time} ${this.greetingMessage}</div>
+        <div class="header">
+            <span class="greeting">${this.greetingMessage}</span>
+            <span class="time">${this.time}</span>
+        </div>
         `;
     }
 }
